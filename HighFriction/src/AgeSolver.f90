3c62db5a7ad1626bc98a7bdc6340c6c44087455e
Clara Henry <clara.henry@mpimet.mpg.de>
2022-03-24 10:41:14 +0100

    Working version (except potentially: restart code structure)

--- HighFriction/src/AgeSolver.f90
+++ HighFriction/src/AgeSolver.f90
@@ -2,19 +2,19 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
   !------------------------------------------------------------------------------
   !******************************************************************************
   !  Calculate Age. SemiLagrangian Method
-  !  Notes:
+  !  Notes: 
   !  Reference to Martin and Gudmundsson (2012), Effects on nonlinear rheology and anisotropy on the
-  !  relationship between age and depth at ice divides, would be appreciated.
+  !  relationship between age and depth at ice divides, would be appreciated.  
   !
   !  Numerical details:
-  !  This solver implement a semilagrangian algoritm with a two-time-level scheme and linear interpolation.
-  !  It is based in Staniforth, Andrew, Jean Côté, 1991: Semi-Lagrangian Integration Schemes for Atmospheric
+  !  This solver implement a semilagrangian algoritm with a two-time-level scheme and linear interpolation. 
+  !  It is based in Staniforth, Andrew, Jean Côté, 1991: Semi-Lagrangian Integration Schemes for Atmospheric 
   !  Models—A Review. Mon. Wea. Rev., 119, 2206–2223.
-  !
+  ! 
   !  Notes for Reinhard:
   !  I have done 3 shortcuts
   !  -1- As usual, only works under CFL conditions. dt<dx/v. No plans to improve that unless we find a real
-  !       case for it.
+  !       case for it. 
   !  -2- Inflow BC, it is activated with 'AgeIn=Logical True' in any given BC within the SIF file. It works fine in 2D
   !        BUT in 3D assumes horizontal Advection terms are null (udAgedx=vdAgedy=0) instead of
   !        normal terms (un d Age dn =0). Let's write that in the ToDo list.
@@ -58,10 +58,10 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
 
   REAL(KIND=dp) :: xa(3),xm(3),xd(3),alpha(3),um(3)
   REAL(KIND=dp), DIMENSION(3) :: LocalCoordinates
-  REAL(KIND=dp) :: eps=1.0e-6, eps2=1.0e-6
+  REAL(KIND=dp) :: eps=1.0e-6
   INTEGER :: MaxDOFs
   REAL(KIND=dp), ALLOCATABLE :: Vector(:,:)
-  REAL(KIND=dp) :: Agea,RcvAgea,TmpAge
+  REAL(KIND=dp) :: Agea
 
   REAL(KIND=dp) :: s1(3),s2(3),PoR(3),d2,d2min
   INTEGER :: emin
@@ -71,12 +71,8 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
 
   !Experimental
   LOGICAL, ALLOCATABLE :: Found(:),isBC(:),isAgeIn(:),isAtBoundary(:)
-  LOGICAL, ALLOCATABLE :: SkipThisNodeInThisPartition(:)
-  LOGICAL :: Owner=.FALSE., AtInterface=.FALSE.
   TYPE(ValueList_t), POINTER :: BC
   REAL(KIND=dp), ALLOCATABLE :: Cond(:)
-  INTEGER, DIMENSION(MPI_STATUS_SIZE) :: status
-
 
   INTEGER :: nn,precv
   INTEGER, POINTER :: nlist(:)
@@ -94,7 +90,7 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
 
 
   SAVE FirstTime,NoNeigh,NeighList,Found,isBC,isAgeIn,isAtBoundary
-  SAVE SkipThisNodeInThisPartition
+
   WRITE(Message,'(a)') 'Start Solver'
   CALL Info('AgeSolver', Message, Level=4)
 
@@ -118,12 +114,12 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
   !    Get variables for the solution
   !------------------------------------------------------------------------------
 
-  Age     => Solver % Variable % Values     ! Nodal values for
-  AgePerm => Solver % Variable % Perm       ! Permutations for
+  Age     => Solver % Variable % Values     ! Nodal values for 
+  AgePerm => Solver % Variable % Perm       ! Permutations for 
   Age0 => Solver % Variable % PrevValues(:,1)
 
   FlowSolName =  GetString( GetEquation(Solver % Mesh % Elements(1)),'Flow Solution Name', GotIt)
-  IF(.NOT.GotIt) THEN
+  IF(.NOT.GotIt) THEN        
      CALL WARN('AgeSolver','Keyword >Flow Solution Name< not found in section >Equation<')
      CALL WARN('AgeSolver','Taking default value >Flow Solution<')
      WRITE(FlowSolName,'(A)') 'Flow Solution'
@@ -132,11 +128,11 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
   IF ( ASSOCIATED( FlowSol ) ) THEN
      FlowPerm     => FlowSol % Perm
      FlowDOFs     =  FlowSol % DOFs
-     Flow         => FlowSol % Values
+     Flow               => FlowSol % Values
   ELSE
      WRITE(Message,'(A,A,A)') &
           'Convection flag set to >computed<, but no variable >',FlowSolName,'< found'
-     CALL FATAL('AgeSolver',Message)
+     CALL FATAL('AgeSolver',Message)              
   END IF
 
   ! Check if the mesh is moving, if so activate the ALE formulation for the velocity
@@ -146,7 +142,7 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
      CALL INFO( 'AgeSolver', 'ALE formualtion activated', Level=4 )
      MeshVelPerm     => MeshVelSol % Perm
      MeshVelDOFs     =   MeshVelSol % DOFs
-     MeshVel         => MeshVelSol % Values
+     MeshVel               => MeshVelSol % Values
   ELSE
      ALE = .FALSE.
      CALL INFO( 'AgeSolver', 'ALE formualtion NOT activated', Level=4 )
@@ -201,15 +197,15 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
 
               IF(.NOT.IsThere) THEN
                  NoNeigh(k)= NoNeigh(k) + 1
-                 NeighList(k, NoNeigh(k))=e
+                 NeighList(k, NoNeigh(k))=e                 
               END IF
            END IF
         END DO
 
      END DO
 
-     ! Flag nodes that are in BC with AgeIn.
-     !(We will assume that the horizontal gradient of age is null and ignore the horizontal components of velocity.)
+     ! Flag nodes that are in BC with AgeIn. 
+     !(We will assume that the horizontal gradient of age is null and ignore the horizontal components of velocity.) 
      ALLOCATE(isAgeIn(NM),                  &
           STAT=stat  )
      IF ( stat /= 0 ) THEN
@@ -226,7 +222,7 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
         IF(ListCheckPresent(BC,'AgeIn'))  THEN
 
            DO j=1,Model % NumberOfBCs
-              IF ( Element % BoundaryInfo % Constraint == Model % BCs(j) % Tag ) THEN
+              IF ( Element % BoundaryInfo % Constraint == Model % BCs(j) % Tag ) THEN                  
                  !                print *,ListGetLogical(Model % BCs(j) % Values,'AgeIn', gotIt )
                  isAgeIn(Nodeindexes)= &
                       ListGetLogical(Model % BCs(j) % Values,'AgeIn', gotIt )
@@ -241,12 +237,6 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
      ! Flag nodes that are Dirichlet Boundary conditions
 
      ALLOCATE(isBC(NM),isAtBoundary(NM),                  &
-          STAT=stat  )
-     IF ( stat /= 0 ) THEN
-        CALL Fatal('AgeSolver','Memory allocation error, Aborting.')
-     END IF
-
-     ALLOCATE(SkipThisNodeInThisPartition(NM),                  &
           STAT=stat  )
      IF ( stat /= 0 ) THEN
         CALL Fatal('AgeSolver','Memory allocation error, Aborting.')
@@ -271,10 +261,10 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
            IF(ListCheckPresent(BC,Trim(Solver % Variable % Name)//' Condition'))  THEN
 
               DO j=1,Model % NumberOfBCs
-                 IF ( Element % BoundaryInfo % Constraint == Model % BCs(j) % Tag ) THEN
+                 IF ( Element % BoundaryInfo % Constraint == Model % BCs(j) % Tag ) THEN                  
                     isBC(Nodeindexes)= &
                          (ListGetReal(Model % BCs(j) % Values,&
-                         Trim(Solver % Variable % Name)//' Condition',n, NodeIndexes, gotIt )>=0.0d0)
+                         Trim(Solver % Variable % Name)//' Condition',n, NodeIndexes, gotIt )>=0.0d0)   
                  END IF
               END DO
            ELSE
@@ -292,12 +282,11 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
   totst = 0.0_dp
   at = CPUTime()
   CALL INFO( 'AgeSolver', 'start assembly', Level=4 )
-  CALL DefaultInitialize()
 
+  CALL DefaultInitialize()
   CALL DefaultFinishAssembly()
 
   Norm=0.0d0
-  SkipThisNodeInThisPartition=.FALSE.
 
   DO k=1,NM
      Found(k)=.FALSE.
@@ -307,14 +296,14 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
         CYCLE
      END IF
 
-
+     
      ka=AgePerm(k)
      kf=FlowPerm(k)
      km=MeshVelPerm(k)
 
      xa(1) = Solver % Mesh % Nodes % x(k)
      xa(2) = Solver % Mesh % Nodes % y(k)
-     xa(3) = Solver % Mesh % Nodes % z(k)
+     xa(3) = Solver % Mesh % Nodes % z(k) 
 
      IF(ALE) THEN
         IF(DIM==2.AND.FlowDOFs==3) THEN
@@ -349,22 +338,15 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
      END IF
 
      xm(1:3)=xa(1:3)-alpha(1:3)/2._dp
-
+  
      IsThere=PointInDomain(Element, ElementNodes, NodeIndexes, n, xm, LocalCoordinates,eps, &
           k,Solver,NoNeigh,NeighList)
 
-     !! Note To Carlos: Putting xm to the closest node is done for all partition boundaries,
-     !! (i.e. AtInterface == TRUE) which I don't think is what we want.
-     Owner = (Solver %  Mesh % ParallelInfo % NeighbourList(k) % Neighbours(1) == ParEnv % MyPE)
-     ! IF (ParEnv % PEs .gt. 1) THEN
-        AtInterface = Solver % Mesh % ParallelInfo % INTERFACE(k)   !! [VV]
-     ! END IF
-
      IF(.NOT.IsThere) THEN
-         IsThere=ClosestPointInBoundary(Element, ElementNodes, NodeIndexes, n, xm, LocalCoordinates,eps2, &
+        IsThere=ClosestPointInBoundary(Element, ElementNodes, NodeIndexes, n, xm, LocalCoordinates,eps, &
              k,Solver,NoNeigh,NeighList)
      END IF
-
+     
      DO j=1,DIM
         IF(ALE) THEN
            Vector(j,1:n)=Flow(FlowDOFs*(FlowPerm(NodeIndexes)-1)+j)&
@@ -389,57 +371,30 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
 
      alpha(1)=Dt*um(1)
      alpha(2)=Dt*um(2)
-     alpha(3)=Dt*um(3)
+     alpha(3)=Dt*um(3)           
 
      xd(1)=xa(1)-alpha(1)
      xd(2)=xa(2)-alpha(2)
      xd(3)=xa(3)-alpha(3)
-
+     
      IsThere=PointInDomain(Element, ElementNodes, NodeIndexes, n, xd, LocalCoordinates,eps, &
           k,Solver,NoNeigh,NeighList)
      IF(.NOT.IsThere) THEN
-        IsThere=ClosestPointInBoundary(Element, ElementNodes, NodeIndexes, n, xd, LocalCoordinates,eps2, &
+        IsThere=ClosestPointInBoundary(Element, ElementNodes, NodeIndexes, n, xd, LocalCoordinates,eps, &
              k,Solver,NoNeigh,NeighList)
      END IF
 
 
 
-
+     
 !!!! Should be the solution in the previous time-step Age0 but let's do it this way for now
-     Vector(1,1:n)=Age(AgePerm(Element % NodeIndexes))
+     Vector(1,1:n)=Age(AgePerm(Element % NodeIndexes))     
 
      Agea=InterpolateInElement(Element,Vector(1,1:n), LocalCoordinates(1), &
           LocalCoordinates(2), LocalCoordinates(3) )+Dt
 
-    !! Note to Carlos: I noticed that in my specific case different ages were calculated
-    !! for nodes belonging to more than one partition. This is a quickfix harmonizing the
-    !! ages across paritions. I take the minimum Age because in my case the vertical
-    !! streaks were alwasy too old. This helps with the symptom, but is not the cause.
-    !!___________________________________________________________________________________________________________________
-    IF (1 == 1) THEN
-    IF (AtInterface .AND. ParEnv % PEs .gt. 1) THEN
-       DO i=1,SIZE(Solver %  Mesh % ParallelInfo % NeighbourList(k) % Neighbours)
-         IF (Solver %  Mesh % ParallelInfo % NeighbourList(k) % Neighbours(i) == ParEnv % MyPE) CYCLE
-         !Send Age to neighboring partition(s)
-         CALL MPI_BSEND(Agea, 1, MPI_DOUBLE, Solver %  Mesh % ParallelInfo % NeighbourList(k) % Neighbours(i), 200,MPI_COMM_WORLD, ierr)
-
-       END DO
-       TmpAge=Agea
-       DO i=1,SIZE(Solver %  Mesh % ParallelInfo % NeighbourList(k) % Neighbours)
-         IF (Solver %  Mesh % ParallelInfo % NeighbourList(k) % Neighbours(i) == ParEnv % MyPE) CYCLE
-         !Receive Age from neighboring partition(s)
-         CALL MPI_RECV(RcvAgea, 1, MPI_DOUBLE, Solver %  Mesh % ParallelInfo % NeighbourList(k) % Neighbours(i), 200,MPI_COMM_WORLD,status, ierr)
-         IF (RcvAgea .lt. TmpAge) THEN
-             TmpAge = RcvAgea
-         END IF
-       END DO
-       Agea = TmpAge
-    END IF
-    END IF
-    !!END________________________________________________________________________________________________________________
-
      Found(k)=.TRUE.
-     CALL SetMatrixElement( SystemMatrix, ka, ka, 1.0d0 )
+     CALL SetMatrixElement( SystemMatrix, ka, ka, 1.0d0 ) 
      SystemMatrix % RHS(ka) = Agea
   END DO
 
@@ -451,7 +406,7 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
   !    Solve System  and check for convergence
   !------------------------------------------------------------------------------
   at = CPUTime() - at
-  st = CPUTime()
+  st = CPUTime() 
 
   PrevNorm = Solver % Variable % Norm
 
@@ -472,7 +427,7 @@ SUBROUTINE AgeSolver( Model,Solver,dt,TransientSimulation )
   FirstTime=.FALSE.
 
 CONTAINS
-
+   
    FUNCTION  ClosestPointInBoundary(Element,ElementNodes,NodeIndexes,n, xx, LocalCoordinates,eps&
         ,k,Solver,NoNeigh,NeighList) RESULT(IsThere)
      IMPLICIT NONE
@@ -498,7 +453,7 @@ CONTAINS
            yy=(/Solver % Mesh % Nodes % x( NodeIndexes(j)),&
                 Solver % Mesh % Nodes % y( NodeIndexes(j)),&
                 Solver % Mesh % Nodes % z( NodeIndexes(j))/)
-
+           
            dist2=DistanceSquared(xx,yy)
            IF(dist2<dist2min) THEN
               dist2min=dist2
@@ -506,12 +461,12 @@ CONTAINS
            END IF
         END DO
      END DO
-
+     
      IsThere=PointInDomain(Element, ElementNodes, NodeIndexes, n, yymin, LocalCoordinates,eps, &
           k,Solver,NoNeigh,NeighList)
 
      IF (.NOT.IsThere) CALL FATAL('AgeSolver','Cannot find closest node to a point. Stop and cry!')
-
+     
    END FUNCTION ClosestPointInBoundary
 
 
@@ -535,7 +490,7 @@ CONTAINS
 
         n = Element % Type % NumberOfNodes
         NodeIndexes => Element % NodeIndexes
-
+        
         ElementNodes % x(1:n) = Solver % Mesh % Nodes % x( NodeIndexes)
         ElementNodes % y(1:n) = Solver % Mesh % Nodes % y( NodeIndexes)
         ElementNodes % z(1:n) = Solver % Mesh % Nodes % z( NodeIndexes)
@@ -545,16 +500,18 @@ CONTAINS
            EXIT
         END IF
      END DO
-
+    
    END FUNCTION PointInDomain
-
+   
    !Square of the distance between 2 points P1 and P2
    FUNCTION DistanceSquared(p1,p2) RESULT(d2)
      IMPLICIT NONE
      DOUBLE PRECISION :: p1(:),p2(:),d2
-
+     
      d2=(p2(1)-p1(1))*(p2(1)-p1(1))+(p2(2)-p1(2))*(p2(2)-p1(2))+(p2(3)-p1(3))*(p2(3)-p1(3))
-
+     
    END FUNCTION DistanceSquared
 
 END SUBROUTINE AgeSolver
+
+
